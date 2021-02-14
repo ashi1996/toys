1e9383d037ed321df849184b64633f3eb0ed2932
ashi mor <ashimor1234@gmail.com>
2021-02-14 16:58:51 +0200

    ashi1

---
+++ public/app.js
@@ -0,0 +1,50 @@
+window.onload = () =>{
+  viewEvent();  
+  
+}
+
+function viewEvent() {
+  
+  document.querySelectorAll('#left').forEach(item => {
+    item.addEventListener('mouseover', function() {
+
+  
+      let box = this.dataset.box
+      document.querySelector(`#${box}`).style.transform = "scale(1.1)"; 
+      
+    })
+  })
+
+  document.querySelectorAll('#left').forEach(item => {
+    item.addEventListener('mouseleave', function() {
+
+      let box = this.dataset.box
+      // alert(box)
+      document.querySelector(`#${box}`).style.transform = "scale(1.0)"; 
+
+  
+    })
+  })
+
+
+
+
+
+
+
+
+  // document.querySelector('#left').addEventListener("mouseover" , (e) =>{
+
+  //   let box = e.target.dataset.box
+  // alert(box)
+  //   document.querySelector(`#${box}`).style.transform = "scale(1.1)"; 
+
+  // })
+
+  // document.querySelector('#left').addEventListener("mouseleave" , () =>{
+
+  //   document.querySelector(".right").style.transform = "scale(1.0)"; 
+
+  // })
+ 
+}
